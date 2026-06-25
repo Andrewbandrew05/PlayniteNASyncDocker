ff6b75f8e3b066b5e1bc815472bdf50af63239d6
Andrew <andrewhrissler@gmail.com>
2026-06-25 13:50:58 -0400

    Fixing an error in new python server

--- src/server.py
+++ src/server.py
@@ -90,9 +90,9 @@ def status_page():
 # -----------------------------------------------------------------------------
 # Execution
 # -----------------------------------------------------------------------------
-if __name__ in {'__main__', 'pymain'}:
+if __name__ in {'__main__', '__mp_main__'}:
     # Read port from environment variable, fallback to 8080
     port = int(os.environ.get('PORT', 8080))
     
     # storage_secret is required to sign the browser cookies for the login session state
-    ui.run(port=port, host='0.0.0.0', storage_secret='change-this-to-a-secure-key-later')
+    ui.run(port=8080, host='0.0.0.0', storage_secret='change-this-to-a-secure-key-later')
